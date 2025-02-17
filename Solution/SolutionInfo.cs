a0beb4d93f828b660a88d8ceff651282cdd9b391
bm98 <github@mail.burri-web.org>
2025-02-17 20:25:08 +0100

    Update Version Info V0.3

--- Solution/SolutionInfo.cs
+++ Solution/SolutionInfo.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "0.2.6.1" )]
-[assembly: AssemblyFileVersion( "0.2.6.1" )]
+[assembly: AssemblyVersion( "0.3.7.1" )]
+[assembly: AssemblyFileVersion( "0.3.7.1" )]
